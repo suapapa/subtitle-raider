031c486207e7acec17f7622231de9582b6003603
Homin Lee <homin.lee@suapapa.net>
2013-02-27 19:33:49 +0900

    fixed script disapearing bug when change window size

--- sdl.go
+++ sdl.go
@@ -33,15 +33,18 @@ type sdlCtx struct {
 
 func NewSdlContext(w, h int) *sdlCtx {
 	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
-		log.Fatal(sdl.GetError())
+		log.Fatal("failed to init sdl", sdl.GetError())
+		return nil
+	}
+
+	if ttf.Init() != 0 {
+		log.Fatal("failed to init ttf", sdl.GetError())
 		return nil
 	}
 
 	var ctx sdlCtx
-	ctx.surface = sdl.SetVideoMode(w, h, 32, sdl.RESIZABLE) /* sdl.FULLSCREEN */
-	if ctx.surface == nil {
-		log.Fatal(sdl.GetError())
-		return nil
+	if err := ctx.setSurface(w, h); err != nil {
+		log.Fatal(err)
 	}
 
 	var vInfo = sdl.GetVideoInfo()
@@ -49,40 +52,40 @@ func NewSdlContext(w, h int) *sdlCtx {
 	log.Println("WM_available = ", vInfo.WM_available)
 	log.Println("Video_mem = ", vInfo.Video_mem, "kb")
 
-	sdl.EnableUNICODE(1)
-
-	if ttf.Init() != 0 {
-		log.Fatal("failed to init ttf", sdl.GetError())
-	}
-
 	title := "Subtitle Player"
 	icon := "" // path/to/icon
 	sdl.WM_SetCaption(title, icon)
 
+	sdl.EnableUNICODE(1)
+
 	go func() {
+	EVENT_LOOP:
 		for {
-			select {
-			case event := <-sdl.Events:
-				/* log.Printf("%#v\n", event) */
-				switch e := event.(type) {
-				case sdl.QuitEvent:
-					return
-				case sdl.KeyboardEvent:
-					log.Printf("Sim:%08x, Mod:%04x\n",
-						e.Keysym.Sym, e.Keysym.Mod)
-				case sdl.ResizeEvent:
-					screen := sdl.SetVideoMode(int(e.W), int(e.H),
-						32, sdl.RESIZABLE)
-					if screen == nil {
-						log.Fatal(sdl.GetError())
-					}
-					ctx.surface = screen
-				}
+			err := ctx.handelEvent()
+			if err != nil {
+				fmt.Println(err)
+				break EVENT_LOOP
 			}
 		}
+		log.Println("sdl: exit event loop")
 	}()
 
 	return &ctx
+}
+
+func (c *sdlCtx) Release() {
+	if c.font != nil {
+		c.font.Close()
+	}
+	if c.surface != nil {
+		c.surface.Free()
+	}
+	ttf.Quit()
+	sdl.Quit()
+}
+
+func (c *sdlCtx) DisplayScript(script *srt.Script) {
+	c.displayScript(script, true, false)
 }
 
 func (c *sdlCtx) Clear() {
@@ -95,14 +98,63 @@ func (c *sdlCtx) Clear() {
 	c.currScript = nil
 }
 
-func (c *sdlCtx) DisplayScript(script *srt.Script) {
-	if c.currScript == script {
+func (c *sdlCtx) SetFont(path string, size int) error {
+	c.font = ttf.OpenFont(path, size)
+	if c.font == nil {
+		errMsg := fmt.Sprintf("failed to open font from %s: %s",
+			path, sdl.GetError())
+		return errors.New(errMsg)
+	}
+	c.FontSize = size
+	c.lineHeight = c.font.LineSkip()
+	/* ctx.font.SetStyle(ttf.STYLE_UNDERLINE) */
+	return nil
+}
+
+func (c *sdlCtx) setSurface(w, h int) error {
+	log.Printf("setSurface to %dx%d", w, h)
+	c.surface = sdl.SetVideoMode(w, h, 32, sdl.RESIZABLE) /* sdl.FULLSCREEN */
+	if c.surface == nil {
+		errMsg := fmt.Sprintf("sdl: failed to set video to %dx%d: %s",
+			w, h, sdl.GetError())
+		return errors.New(errMsg)
+	}
+
+	c.w, c.h = w, h
+	if c.currScript != nil {
+		c.displayScript(c.currScript, false, true)
+	}
+
+	return nil
+}
+
+func (c *sdlCtx) handelEvent() error {
+	select {
+	case event := <-sdl.Events:
+		/* log.Printf("%#v\n", event) */
+		switch e := event.(type) {
+		case sdl.QuitEvent:
+			return errors.New("sdl: received QuitEvent")
+		case sdl.KeyboardEvent:
+			log.Printf("Sim:%08x, Mod:%04x, Unicode:%02x\n",
+				e.Keysym.Sym, e.Keysym.Mod, e.Keysym.Unicode)
+		case sdl.ResizeEvent:
+			if err := c.setSurface(int(e.W), int(e.H)); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	return nil
+}
+
+func (c *sdlCtx) displayScript(script *srt.Script,
+	andClear bool, forceUpdate bool) {
+	if forceUpdate == false && c.currScript == script {
 		return
 	}
 	c.currScript = script
 
 	log.Printf("display %s", script.Text)
-	timer := time.NewTimer(script.Duration())
 
 	if c.font == nil {
 		log.Println("set default font")
@@ -123,33 +175,14 @@ func (c *sdlCtx) DisplayScript(script *srt.Script) {
 	c.surface.Blit(&sdl.Rect{0, 0, 0, 0}, glypse, nil)
 	c.surface.Flip()
 
+	if andClear == false {
+		return
+	}
+
+	timer := time.NewTimer(script.Duration())
 	<-timer.C
 	if c.currScript == script {
 		c.surface.FillRect(nil, 0 /* BG_COLOR */)
 		c.surface.Flip()
 	}
-}
-
-func (c *sdlCtx) SetFont(path string, size int) error {
-	c.font = ttf.OpenFont(path, size)
-	if c.font == nil {
-		errMsg := fmt.Sprintf("failed to open font from %s: %s",
-			path, sdl.GetError())
-		return errors.New(errMsg)
-	}
-	c.FontSize = size
-	c.lineHeight = c.font.LineSkip()
-	return nil
-}
-
-/* ctx.font.SetStyle(ttf.STYLE_UNDERLINE) */
-func (c *sdlCtx) Release() {
-	if c.font != nil {
-		c.font.Close()
-	}
-	if c.surface != nil {
-		c.surface.Free()
-	}
-	ttf.Quit()
-	sdl.Quit()
 }
