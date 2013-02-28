0088946ab4bca33b9ae9c57a379158aa17f45caf
Homin Lee <suapapa@insignal.co.kr>
2013-02-28 13:20:44 +0900

    subtitle: Add markup strip

--- subtitle/subtitle.go
+++ subtitle/subtitle.go
@@ -1,17 +1,24 @@
 package subtitle
 
 import (
+	"regexp"
 	"time"
 )
 
+var reMakrup = regexp.MustCompile("</?[^<>]+?>")
+
 type Script struct {
-	Idx int
+	Idx        int
 	Start, End time.Duration
-	Text string
+	Text       string
 }
 
 func (s *Script) Duration() time.Duration {
 	return s.End - s.Start
+}
+
+func (s *Script) TextWithoutMarkup() string {
+	return reMakrup.ReplaceAllString(s.Text, "")
 }
 
 type Book []Script
