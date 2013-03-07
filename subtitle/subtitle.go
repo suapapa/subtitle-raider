1f2423f71b691031f3a16e9f43b370373447fc04
Homin Lee <homin.lee@suapapa.net>
2013-03-07 16:48:56 +0900

    subtitle: Check timestamp on a script

--- subtitle/subtitle.go
+++ subtitle/subtitle.go
@@ -9,20 +9,47 @@ import (
 	"time"
 )
 
-var reMakrup = regexp.MustCompile("</?[^<>]+?>")
+// Collection of scripts
+type Book []Script
 
+// A script
 type Script struct {
 	Idx        int
 	Start, End time.Duration
 	Text       string
 }
 
+// How long the script should be shown
 func (s *Script) Duration() time.Duration {
 	return s.End - s.Start
 }
 
+// Script HTML markup from text
 func (s *Script) TextWithoutMarkup() string {
 	return reMakrup.ReplaceAllString(s.Text, "")
 }
 
-type Book []Script
+// Check the script with given timestamp
+func (s *Script) CheckHit(ts time.Duration) HitStatus {
+	switch {
+	case ts < s.Start:
+		return HS_EARLY
+	case ts >= s.Start && s.End >= s.End:
+		return HS_HIT
+	case s.End < ts:
+		return HS_LATE
+	}
+	return HS_INVALID
+}
+
+// HitStatus is type for timestamp check
+type HitStatus uint8
+
+const (
+	HS_INVALID HitStatus = iota
+	HS_EARLY             // Not yet
+	HS_HIT               // Now
+	HS_LATE              // Gone
+)
+
+var reMakrup = regexp.MustCompile("</?[^<>]+?>")
