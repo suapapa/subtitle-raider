cb8da742e0b41fdb3987ff1fa84a7eafcb2d0980
Homin Lee <homin.lee@suapapa.net>
2013-03-10 19:34:36 +0900

    Clean up main.go

--- main.go
+++ main.go
@@ -51,81 +51,76 @@ func main() {
 			navC <- opt.startIdx
 		}()
 	}
+
 CHAN_LOOP:
-	for {
-		select {
-		case nav := <-navC:
-			currScriptIdx += nav
-			if currScriptIdx < 0 {
-				currScriptIdx = 0
-			} else if currScriptIdx >= len(book) {
-				currScriptIdx = len(book) - 1
-			}
-			if nav == 0 {
-				paused = !paused
-			}
-
-			currScript := &book[currScriptIdx]
-			nextScript = nil
-			screen.DisplayScript(currScript)
-			if paused == false {
-				startTime = time.Now()
-				tsVias = currScript.Start
-				tsClear = currScript.End
-			} else {
-				tsClear = 0
-			}
-
-		case v := <-tsViasC:
-			tsVias += v
-
-		case <-debugTkr.C:
-			if nextScript == nil {
-				continue CHAN_LOOP
-			}
-			tsCurr := time.Since(startTime)
-			debugStr := fmt.Sprintf("%d:%s(%s...%s) ClearTs=%s Ts=%s",
-				nextScript.Idx, nextScript.Text,
-				nextScript.Start, nextScript.End,
-				tsClear, tsCurr+tsVias)
-			screen.displayDebug(debugStr)
-			/* screen.displayDebug("debug display") */
-
-		case <-tkr.C:
-			if paused {
-				continue
-			}
-
-			tsCurr := time.Since(startTime)
-			tsCurr += tsVias
-			if tsCurr < 0 {
-				nextScript = &book[0]
-				continue
-			}
-
-			if nextScript == nil {
-				nextScript = book.Find(tsCurr)
-			}
-
-			if nextScript != nil {
-				if hs := nextScript.CheckHit(tsCurr); hs == subtitle.SCR_HIT {
-					screen.DisplayScript(nextScript)
-					tsClear = nextScript.End
-					currScriptIdx = nextScript.Idx - 1
-					nextScript = nil
-				}
-			}
-
-			if tsClear != 0 && tsClear <= tsCurr {
-				screen.Clear()
-				tsClear = 0
-			}
-
-		case <-quitC:
-			screen.Release()
-			log.Println("Bye Bye")
-			os.Exit(0)
-
+	select {
+	case nav := <-navC:
+		currScriptIdx += nav
+		if currScriptIdx < 0 {
+			currScriptIdx = 0
+		} else if currScriptIdx >= len(book) {
+			currScriptIdx = len(book) - 1
 		}
+		if nav == 0 {
+			paused = !paused
+		}
+
+		currScript := &book[currScriptIdx]
+		nextScript = nil
+		screen.DisplayScript(currScript)
+		if paused == false {
+			startTime = time.Now()
+			tsVias = currScript.Start
+			tsClear = currScript.End
+		} else {
+			tsClear = 0
+		}
+
+	case v := <-tsViasC:
+		tsVias += v
+
+	case <-debugTkr.C:
+		tsCurr := time.Since(startTime)
+
+		debugStr := fmt.Sprintf("%s ClearTs=%s Ts=%s",
+			nextScript, tsClear, tsCurr+tsVias)
+		screen.displayDebug(debugStr)
+
+	case <-tkr.C:
+		if paused {
+			break
+		}
+
+		tsCurr := time.Since(startTime)
+		tsCurr += tsVias
+		if tsCurr < 0 {
+			nextScript = &book[0]
+			break
+		}
+
+		if nextScript == nil {
+			nextScript = book.Find(tsCurr)
+		}
+
+		if nextScript != nil {
+			if subtitle.SCR_HIT == nextScript.CheckHit(tsCurr) {
+				screen.DisplayScript(nextScript)
+				tsClear = nextScript.End
+				currScriptIdx = nextScript.Idx - 1
+				nextScript = nil
+			}
+		}
+
+		if tsClear != 0 && tsClear <= tsCurr {
+			screen.Clear()
+			tsClear = 0
+		}
+
+	case <-quitC:
+		screen.Release()
+		log.Println("Bye Bye")
+		os.Exit(0)
+
 	}
+	goto CHAN_LOOP
 }
