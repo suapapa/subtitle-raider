b80039b15af80acc45a2f3cc9e7ad3a01cee5d4b
Homin Lee <homin.lee@suapapa.net>
2013-03-20 15:44:54 +0900

    subtitle: add document for the package

---
+++ subtitle/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2013, Homin Lee. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package subtitle probive structure for movie subtitle.
+// Read subtitle book srt file and check current -or next- subtitle for
+// given timestamp.
+package subtitle
