6b1b072c3d904d1e9e93bb6655e3dc90e81be9d4
Homin Lee <homin.lee@suapapa.net>
2013-03-20 16:18:49 +0900

    subtitle: update package document

--- subtitle/doc.go
+++ subtitle/doc.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package subtitle probive structure for movie subtitle.
-// Read subtitle book srt file and check current -or next- subtitle for
-// given timestamp.
+// Read subtitle book from srt file.
+// And, provide fuctions to display each script on time.
 package subtitle
